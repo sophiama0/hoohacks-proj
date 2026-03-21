7ed8b8489ff990b29da2df763618a112a0f88e21
Tyler Nguyen <wgq4tr@virginia.edu>
2026-03-21 14:21:28 -0400

    connected supabase client

---
+++ supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+const supabaseUrl = 'https://sebuzpqurijtvbqgfvel.supabase.co'
+const supabaseAnonKey = 'sb_publishable_oLgvQAN7DGQMRH49LvuMEA_yPGrPDSi'
+
+window.supabase = createClient(supabaseUrl, supabaseAnonKey)
